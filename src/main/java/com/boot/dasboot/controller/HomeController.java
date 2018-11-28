381c49aad8964867d2dabff41a12ff838ded049a
waqasdilawar <waqas.dilawar@discorp.com>
2018-11-28 21:14:29 +0500

    Added post mapping

--- src/main/java/com/boot/dasboot/controller/HomeController.java
+++ src/main/java/com/boot/dasboot/controller/HomeController.java
@@ -1,8 +1,6 @@
 package com.boot.dasboot.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HomeController {
@@ -13,5 +11,9 @@ public class HomeController {
     @GetMapping("/home")
     public String get(){
         return "Idea";
+    }
+    @PostMapping("/home")
+    public String post(@RequestBody String message){
+        return message;
     }
 }
