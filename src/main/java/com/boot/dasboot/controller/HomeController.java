1505bc3eb9fdc3061f0e94b824ff55f2a8bf2aa7
waqasdilawar <waqas.dilawar@discorp.com>
2018-11-28 20:40:46 +0500

    Added project files, added few packages

---
+++ src/main/java/com/boot/dasboot/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.boot.dasboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    public String home(){
+        return  "Das, reporting for body";
+    }
+}
