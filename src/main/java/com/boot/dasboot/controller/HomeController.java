fcbd7bf29c40ba9016883909e51df4729fc17743
waqasdilawar <waqas.dilawar@discorp.com>
2018-11-28 20:57:00 +0500

    Added get mapping

--- src/main/java/com/boot/dasboot/controller/HomeController.java
+++ src/main/java/com/boot/dasboot/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.boot.dasboot.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,9 @@ public class HomeController {
     @RequestMapping("/")
     public String home(){
         return  "Das, reporting for body";
+    }
+    @GetMapping("/home")
+    public String get(){
+        return "Idea";
     }
 }
